c8ab175433327864f412eff8545ef0b9b7987d82
leanderkretschmer <leanderkretschmer@icloud.com>
2025-08-20 09:19:10 +0200

    version 0.4.10

--- lib/calendar_subscription/hooks.rb
+++ lib/calendar_subscription/hooks.rb
@@ -2,5 +2,6 @@ module CalendarSubscription
   class Hooks < Redmine::Hook::ViewListener
     render_on :view_issues_index_bottom, :partial => 'calendar_subscription/other_format_link'
     render_on :view_projects_show_sidebar_bottom, :partial => 'calendar_subscription/sidebar'
+    render_on :view_users_form, :partial => 'calendar_subscription/admin_user_button'
   end
 end
